3119f791924c25b5f3c7bf912dc1d5d5d0a39a0e
Dhiman Dasgupta <dhiman.dasgupta1984@gmail.com>
2022-02-24 23:26:07 +0530

    Updating all dependency versions.

--- buildSrc/src/main/java/com/dhimandasgupta/buildsrc/Hilt.kt
+++ buildSrc/src/main/java/com/dhimandasgupta/buildsrc/Hilt.kt
@@ -1,7 +1,7 @@
 package com.dhimandasgupta.buildsrc
 
 object Hilt {
-    const val VERSION = "2.40.3"
+    const val VERSION = "2.41"
 
     const val KAPT = "com.google.dagger:hilt-compiler:$VERSION"
     const val HILT_ANDROID = "com.google.dagger:hilt-android:$VERSION"
--- buildSrc/src/main/java/com/dhimandasgupta/buildsrc/Material3.kt
+++ buildSrc/src/main/java/com/dhimandasgupta/buildsrc/Material3.kt
@@ -1,7 +1,7 @@
 package com.dhimandasgupta.buildsrc
 
 object Material3 {
-    private const val VERSION = "1.0.0-alpha01"
+    private const val VERSION = "1.0.0-alpha06"
 
     const val MATERIAL3 = "androidx.compose.material3:material3:$VERSION"
 }
